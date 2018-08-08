5aea12f0144f5af4266c8cb48fa5898d8aecb69b
Ivan <panchenkovanya@gmail.com>
2018-08-08 13:14:36 +0000

    add a validation price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+   
     validates :title, :description, :img_url, :presence => true
+    validates :prise, :numericality => {:grater_than_or_equal => 0.01}
 
 end
