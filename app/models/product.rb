f72a8c215031ddc1d21d65ed3512b0cab3a8b78d
Ivan <panchenkovanya@gmail.com>
2018-08-15 11:27:12 +0000

    Change varible prise to price, and make test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
    
     validates :title, :description, :img_url, :presence => true
-    validates :prise, :numericality => {:grater_than_or_equal => 0.01}
+    validates :price, :numericality => {:grater_than_or_equal => 0.01}
     validates :title, :uniqueness => true
     validates :img_url, :format => {
             :with => %r{\.(gif|jpg|png)$}i,
