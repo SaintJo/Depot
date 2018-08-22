59fcd98f7235360cbd01714b921b005257af031b
Ivan <panchenkovanya@gmail.com>
2018-08-22 09:47:33 +0000

    Solve some mistake

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,9 +4,9 @@ class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
     @update = {
-      :title => 'Lorem Ipsum'
-      :description => 'Wibbles are fun'
-      :img_url => 'lorem.jpg'
+      :title => 'Lorem Ipsum',
+      :description => 'Wibbles are fun',
+      :img_url => 'lorem.jpg',
       :prrice => 19.95
     }
   end
@@ -24,7 +24,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: => @update
+      post :create, :product => @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -41,7 +41,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: => @product.to_param, :product => @update
+    put :update, :id => @product.to_param, :product => @update
     assert_redirected_to product_path(assigns(:product))
   end
 
@@ -53,3 +53,4 @@ class ProductsControllerTest < ActionController::TestCase
     assert_redirected_to products_path
   end
 end
+
