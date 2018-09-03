6c52f2761e9f6845fdb383ad7d177755afc9598b
Ivan <panchenkovanya@gmail.com>
2018-09-03 18:55:04 +0000

    add unit test for price and image

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
     validates :title, :description, :img_url, :presence => true
     validates :price, :numericality => {:grater_than_or_equal => 0.01}
     validates :title, :uniqueness => true
+    validates :multiline => true
     validates :img_url, :format => {
             :with => %r{\.(gif|jpg|png)$}i,
             :message => 'must be a URL for GIF,JPG OR PNG image.'
