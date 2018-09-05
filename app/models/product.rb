c5a3da3caf6fe6f8e6b01b92b8fd9b4e5fdccbe7
Ivan <panchenkovanya@gmail.com>
2018-09-05 07:12:32 +0000

    change the image_url to img_url and delete default_scope,change $ to \z in img_url in file product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
    
-    default_scope :oreder => 'title'
+    default_scope :order => 'title'
+
     
     validates :title, :description, :img_url, :presence => true
     validates :price, :numericality => {:grater_than_or_equal => 0.01}
     validates :title, :uniqueness => true
-    validates :multiline => true
     validates :img_url, :format => {
-            :with => %r{\.(gif|jpg|png)$}i,
+            :with => %r{\.(gif|jpg|png)\z}i,
             :message => 'must be a URL for GIF,JPG OR PNG image.'
     }
 end
